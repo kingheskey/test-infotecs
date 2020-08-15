3a61dd285bf380f6cd92c1b5a29bf1cf06e49817
Nikita <nm.kirilin@gmail.comgit>
2020-08-15 20:12:21 +0300

    remove gitignore files

--- import-table.js
+++ import-table.js
@@ -1,10 +1,32 @@
+// Создание структуры таблицы
 
-//Получает json из внешнего источника и добавляет в таблицу
-async function getData(url) {
-    let response = await fetch('./data.json');
-    let jsonData = await response.json();
+/* id таблицы - dataTable, далее id и class не назначаются во избежание конфликта
+    с имеющимися элементами сайта (если представить будто они существуют) */
+document.body.insertAdjacentHTML('afterbegin', '<table id="dataTable"><thead><tr></tr></thead><tbody></tbody></table>');
+const table = document.querySelector('#dataTable');
+const tr = table.querySelector('#dataTable tr');
+const tbody = table.querySelector('tbody');
+document.body.prepend(table);
 
+createHeader();
+getData('./data.json');
+
+// ****ФУНКЦИИ
+
+function createHeader() {
+  const headerTitles = ['Имя', 'Фамилия', 'Описание', 'Цвет глаз'];
+  headerTitles.forEach((title) => tr.insertAdjacentHTML('beforeend', `<th>${title}</th>`));
 }
 
-getData('.data.json');
+// Добавление информации из json объекта в таблицу
+async function getData(url) {
+  const response = await fetch(url);
+  const jsonData = await response.json();
 
+  jsonData.forEach((elem) => tbody.insertAdjacentHTML('beforeend',
+    `<tr>
+    <td class='sort'>${elem.name.firstName}</td>
+    <td class='sort'>${elem.name.lastName}</td>
+    <td class='sort'><div class ='about'>${elem.about}</div></td>
+    <td class='sort'>${elem.eyeColor}</td>`));
+}
