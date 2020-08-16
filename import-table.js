3e08715125932400e6cd0cd38a2073f4ad301e9a
Nikita <nm.kirilin@gmail.comgit>
2020-08-17 00:26:46 +0300

    added column sorting function + existing code refactoring w/ comments adding

--- import-table.js
+++ import-table.js
@@ -1,32 +1,90 @@
-// Создание структуры таблицы
+'use strict'
 
-/* id таблицы - dataTable, далее id и class не назначаются во избежание конфликта
-    с имеющимися элементами сайта (если представить будто они существуют) */
-document.body.insertAdjacentHTML('afterbegin', '<table id="dataTable"><thead><tr></tr></thead><tbody></tbody></table>');
+// Создание таблицы с id="dataTable" и header id="dataTableHeader"
+document.body.insertAdjacentHTML('afterbegin', '<table id="dataTable"><tr id="dataTableHeader"></tr></table>');
+
+// Глобальные переменные для доступа к элементам таблицы
 const table = document.querySelector('#dataTable');
-const tr = table.querySelector('#dataTable tr');
+const thead = document.querySelector('#dataTableHeader');
 const tbody = table.querySelector('tbody');
-document.body.prepend(table);
 
+document.body.prepend(table);
 createHeader();
 getData('./data.json');
 
-// ****ФУНКЦИИ
+// ***************************
+// Функции
 
+// Создает header таблицы
 function createHeader() {
   const headerTitles = ['Имя', 'Фамилия', 'Описание', 'Цвет глаз'];
-  headerTitles.forEach((title) => tr.insertAdjacentHTML('beforeend', `<th>${title}</th>`));
+  headerTitles.forEach((title) => thead.insertAdjacentHTML('beforeend', `<th onclick="tableSortColumn()">${title}</th>`));
 }
 
 // Добавление информации из json объекта в таблицу
+// {string} url - ссылка на json объект
 async function getData(url) {
   const response = await fetch(url);
   const jsonData = await response.json();
 
+  // Для каждой строки массива jsonData создается строка в таблице по заданному шаблону
+  /* elem.about завернут в div т.к. при назначении display:-webkit-inline-box
+  непосредственно элементу <td>, border элемета <td> отображается некорректно */
   jsonData.forEach((elem) => tbody.insertAdjacentHTML('beforeend',
-    `<tr>
-    <td class='sort'>${elem.name.firstName}</td>
-    <td class='sort'>${elem.name.lastName}</td>
-    <td class='sort'><div class ='about'>${elem.about}</div></td>
-    <td class='sort'>${elem.eyeColor}</td>`));
+    `<tr onclick="selectedRowEdit()">
+    <td>${elem.name.firstName}</td>
+    <td>${elem.name.lastName}</td>
+    <td><div class ='about'>${elem.about}</div></td>
+    <td>${elem.eyeColor}</td>
+    </tr>`));
 }
+
+/* Сортировка колонны по значениям
+Создает классы asc - для сортировки по возрастанию и desc - для сортировки по убыванию */
+function tableSortColumn() {
+  const target = event.currentTarget;
+  const targetId = Array.from(thead.cells).indexOf(target);
+
+  // Первый table.row - header, его пропускаем
+  const sortedRows = Array.from(table.rows).slice(1);
+
+  /* Условие стирает классы asc и desc всех элементов если мы сортируем
+  другой столбец */
+  if (!(target.classList.contains('asc')) || (target.classList.contains('desc'))) {
+    Array.from(thead.children).forEach((element) => {
+      element.classList.remove('asc', 'desc');
+    });
+  }
+  /*
+  1. При первом использовании сортировки и при переходе на другие столбцы
+  устанавливается class = "asc" => 1 условие;
+  2. При повторном использовании toggle стирает class = "asc" с элемента => 2 условие,
+  устанавливает class="desc";
+  3. При третьем использовании toggle меняет "desc" на "asc" => 1 условие */
+  target.classList.toggle('asc');
+
+  // 1 условие
+  if (target.classList.contains('asc')) {
+    sortedRows.sort((rowA, rowB) => {
+      if (rowA.cells[targetId].innerHTML < rowB.cells[targetId].innerHTML) {
+        return -1;
+      } return 1;
+    });
+    tbody.append(...sortedRows);
+  // 2 условие
+  } else {
+    target.classList.toggle('desc');
+    sortedRows.sort((rowA, rowB) => {
+      if (rowA.cells[targetId].innerHTML > rowB.cells[targetId].innerHTML) {
+        return -1;
+      } return 1;
+    });
+    tbody.append(...sortedRows);
+  }
+}
+
+function selectedRowEdit() {
+  const target = event.currentTarget;
+  document.body.insertAdjacentHTML('beforeend', '<form id="edit-selected"></form>');
+}
+
